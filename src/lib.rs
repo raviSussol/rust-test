ba7048854a5df033d4138e5d53e6583622d10e57
raviSussol <ravi.shrestha@sussol.net>
2023-02-11 13:35:49 +0545

    #16 Create math lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod add;
 pub mod divide;
+pub mod math;
 pub mod multiply;
 pub mod read_file;
 pub mod subtract;
--- tests/divide_test.rs
+++
@@ -1,9 +0,0 @@
-use rust_test::divide::divide;
-
-#[test]
-fn divide_test() {
-    assert_eq!(divide(10, 5), 2);
-}
-// update test comment...
-// second update test comment...
-//
